2dba303ea3585eba3780f49788efe4ae9e81c7dd
Lukelowy <luke.lowy@gmail.com>
2021-09-27 11:52:39 +1000

    Completed all the steps for section 7.1 but I am unsure about somethings e.g. trying to understand the query and how to implement the raw SQL into ManageBookings/EditFirst commit

---
+++ Models/Booking.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hotelone19301408.Models
+{
+    public class Booking
+    {
+
+        [Key, Required]
+        public int ID { get; set; }
+
+        //Romm ID should range between 1 and 16
+        [Display(Name ="Room ID")]
+        [Range(1, 16)]
+        public int RoomID { get; set; }
+
+        [Display(Name ="Customer Email")]
+        [DataType(DataType.EmailAddress)]
+        public string CustomerEmail { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [Display(Name ="Check In Date")]
+        public DateTime CheckIn { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Check Out Date")]
+        public DateTime CheckOut { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal Cost { get; set; }
+
+        public Room TheRoom { get; set; }
+
+        [Display(Name ="Customer Name")]
+        public Customer TheCustomer { get; set; }
+    }
+}
---
+++ Pages/ManageBookings/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Hotelone19301408.Data;
+using Hotelone19301408.Models;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace Hotelone19301408.Pages.ManageBookings
+{
+    [Authorize (Roles = "administrators")]
+    public class IndexModel : PageModel
+    {
+        private readonly Hotelone19301408.Data.ApplicationDbContext _context;
+
+        public IndexModel(Hotelone19301408.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Booking> Booking { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            //I do not think I shoud use a .inlcude() because the roomID is already in the class 
+            Booking = await _context.Booking
+                .Include(b => b.TheCustomer.Surname).Include(b => b.TheCustomer.GivenName).ToListAsync();
+                //.Include(b => b.RoomID).ToListAsync();
+        }
+    }
+}
