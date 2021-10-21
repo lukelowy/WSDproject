3ce0ec59cb20e5985cf935d92d5615bdffb7531d
sandrakirimi <66510334+sandrakirimi@users.noreply.github.com>
2021-10-22 01:08:36 +1100

    Update SearchViewModel.cs

--- Models/SearchViewModel.cs
+++ Models/SearchViewModel.cs
@@ -6,6 +6,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Hotelone19301408.Models
 {
+[Authorize(Roles = "customers")]
     public class SearchViewModel
     {
         [Display(Name = "Check In Date")]
